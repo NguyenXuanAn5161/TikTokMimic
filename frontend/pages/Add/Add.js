6d9b8cf2373137672f093ec0ab2b821bba31cab3
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2023-11-19 00:40:10 +0700

    add styles for page add

--- frontend/pages/Add/Add.js
+++ frontend/pages/Add/Add.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { View } from "react-native";
 import Camera from "../../components/Camera/Camera";
+import styles from "./AddStyles.js";
 
 const Add = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Camera />
     </View>
   );
