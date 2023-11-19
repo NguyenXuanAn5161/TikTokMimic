3434ae3c32f5e0d4bca6383a914e9d83a75b911d
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2023-11-19 14:44:47 +0700

    create rudux

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,18 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
 import Router from "./Router/Router";
+import Reducers from "./redux/reducers/Reducers";
+
+const store = createStore(Reducers, applyMiddleware(thunk));
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Router />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Router />
+      </NavigationContainer>
+    </Provider>
   );
 }
