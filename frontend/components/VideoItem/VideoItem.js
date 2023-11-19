e038e3eeeb8fe42b2f415bb502cc5f11a250b3de
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2023-11-20 00:49:21 +0700

    finish ui home videoItem

--- frontend/components/VideoItem/VideoItem.js
+++ frontend/components/VideoItem/VideoItem.js
@@ -1,33 +1,75 @@
-import React from "react";
-import { Dimensions, FlatList, Text, View } from "react-native";
+import { ResizeMode, Video } from "expo-av";
+import React, {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from "react";
 import styles from "./VideoItemStyles";
 
-export default function VideoItem() {
-  const data = [1, 2, 3, 4, 5, 6];
+export const VideoItem = forwardRef((props, parentRef) => {
+  const ref = useRef(null);
+  useImperativeHandle(parentRef, () => ({ play, stop, unload }));
 
-  const renderItem = ({ item, index }) => {
-    return (
-      <View
-        style={[
-          { flex: 1, height: Dimensions.get("window").height - 49 },
-          index % 2
-            ? { backgroundColor: "green" }
-            : { backgroundColor: "gray" },
-        ]}
-      >
-        <Text>{item}</Text>
-      </View>
-    );
+  useEffect(() => {
+    return () => unload();
+  }, []);
+
+  const play = async () => {
+    if (ref.current == null) {
+      return;
+    }
+    const status = await ref.current.getStatusAsync();
+    if (status?.isPlaying) {
+      // thuật ngữ Optional Chaining
+      return;
+    }
+    try {
+      await ref.current.playAsync();
+    } catch (e) {
+      console.log(e);
+    }
   };
+
+  const stop = async () => {
+    if (ref.current == null) {
+      return;
+    }
+    const status = await ref.current.getStatusAsync();
+    if (!status?.isPlaying) {
+      return;
+    }
+    try {
+      await ref.current.stopAsync();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const unload = async () => {
+    console.log("unload");
+    if (ref.current == null) {
+      return;
+    }
+    try {
+      await ref.current.unloadAsync();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <FlatList
-        renderItem={renderItem}
-        data={data}
-        pagingEnabled
-        keyExtractor={(item) => item}
-        decelerationRate={"normal"}
-      />
-    </View>
+    <Video
+      ref={ref}
+      style={styles.container}
+      source={{
+        uri: "https://v.pinimg.com/videos/mc/720p/f6/88/88/f68888290d70aca3cbd4ad9cd3aa732f.mp4",
+      }}
+      resizeMode={ResizeMode.CONTAIN}
+      shouldPlay={true}
+      isLooping
+    />
   );
-}
+});
+
+export default VideoItem;
--- frontend/pages/Home/Home.js
+++ frontend/pages/Home/Home.js
@@ -1,11 +1,59 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useRef } from "react";
+import { Dimensions, FlatList, View } from "react-native";
 import VideoItem from "../../components/VideoItem/VideoItem.js";
 import styles from "./HomeStyles.js";
 const Home = () => {
+  const mediaRefs = useRef([]);
+
+  const data = [1, 2, 3, 4, 5, 6];
+
+  const onViewableItemsChanged = useRef(({ changed }) => {
+    changed.forEach((element) => {
+      const cell = mediaRefs.current[element.index];
+      if (cell) {
+        console.log("onViewableItemsChanged", element, element.isViewable);
+        if (element.isViewable) {
+          cell.play();
+        } else {
+          cell.stop();
+        }
+      }
+    });
+  });
+
+  const renderItem = ({ item, index }) => {
+    return (
+      <View
+        style={[
+          { flex: 1, height: Dimensions.get("window").height - 49 },
+          index % 2
+            ? { backgroundColor: "green" }
+            : { backgroundColor: "gray" },
+        ]}
+      >
+        <VideoItem
+          ref={(VideoItemRef) => (mediaRefs.current[item] = VideoItemRef)}
+        />
+      </View>
+    );
+  };
   return (
     <View style={styles.container}>
-      <VideoItem />
+      <FlatList
+        renderItem={renderItem}
+        data={data}
+        windowSize={4} // số lượng item được render cùng lúc (xem thêm trong tài liệu)
+        initialNumToRender={1} // số lượng item được render lần đầu
+        maxToRenderPerBatch={2} // số lượng item được render mỗi lần
+        removeClippedSubviews={true} // xóa các item không hiển thị
+        viewabilityConfig={{
+          itemVisiblePercentThreshold: 100, // item được xem khi nào thì play
+        }}
+        pagingEnabled
+        keyExtractor={(item) => item}
+        decelerationRate={"normal"}
+        onViewableItemsChanged={onViewableItemsChanged.current}
+      />
     </View>
   );
 };
