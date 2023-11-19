92becd4e7cc400af2c83547065aeb540b9ea9a93
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2023-11-19 13:17:31 +0700

    fix safeareaview

--- frontend/Router/Router.js
+++ frontend/Router/Router.js
@@ -2,6 +2,7 @@ import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import * as React from "react";
 import { Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import Add from "../pages/Add/Add";
 import Home from "../pages/Home/Home";
 import Message from "../pages/Message/Message";
@@ -12,74 +13,84 @@ const Tab = createBottomTabNavigator();
 
 const Router = () => {
   return (
-    <Tab.Navigator
-      initialRouteName="Add" // màn hình sẽ hiển thị đầu tiên
-      screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: "#fff",
-        tabBarStyle: {
-          backgroundColor: "#000",
-          padding: 5,
-          justifyContent: "center",
-        },
-      }}
-    >
-      {/* các màn hình lần lượt nằm ở đây */}
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        options={{
-          tabBarLabel: "Trang chủ",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
+    <SafeAreaView style={{ flex: 1, marginHorizontal: 5 }}>
+      <Tab.Navigator
+        initialRouteName="Home" // màn hình sẽ hiển thị đầu tiên
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: "#fff",
+          tabBarStyle: {
+            backgroundColor: "#000",
+            padding: 5,
+            justifyContent: "center",
+          },
         }}
-      />
-      <Tab.Screen
-        name="Shop"
-        component={Shop}
-        options={{
-          tabBarLabel: "Shop",
-          tabBarIcon: ({ color, size }) => (
-            <AntDesign name="shoppingcart" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Add"
-        component={Add}
-        options={{
-          tabBarLabel: "",
-          tabBarIcon: ({ size }) => (
-            <Image
-              source={require("../assets/new-video.png")}
-              style={{ width: size + 20, height: size }}
-            />
-          ),
-        }}
-      />
+      >
+        {/* các màn hình lần lượt nằm ở đây */}
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: "Trang chủ",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Shop"
+          component={Shop}
+          options={{
+            tabBarLabel: "Shop",
+            tabBarIcon: ({ color, size }) => (
+              <AntDesign name="shoppingcart" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Add"
+          component={Add}
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ size }) => (
+              <Image
+                source={require("../assets/new-video.png")}
+                style={{ width: size + 20, height: size }}
+              />
+            ),
+          }}
+        />
 
-      <Tab.Screen
-        name="Message"
-        component={Message}
-        options={{
-          tabBarLabel: "Hộp thư",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="message" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="User"
-        component={User}
-        options={{
-          tabBarLabel: "Hồ sơ",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+        <Tab.Screen
+          name="Message"
+          component={Message}
+          options={{
+            tabBarLabel: "Hộp thư",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="message"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="User"
+          component={User}
+          options={{
+            tabBarLabel: "Hồ sơ",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="account"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </SafeAreaView>
   );
 };
 
